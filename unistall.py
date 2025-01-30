0b8f32ec1bf3fc3f8a5f1f74df0f1d7cfae5a7a4
nolan436 <nono.chome@gmail.com>
2025-01-30 18:12:11 +0100

    Create unistall.py

---
+++ unistall.py
@@ -0,0 +1,10 @@
+import os
+import shutil
+
+def unistall():
+    folder_path = os.path.dirname(os.path.abspath(__file__))
+    print(f"Suppression du dossier : {folder_path}")
+    shutil.rmtree(folder_path)
+
+if __name__ == "__main__":
+    unistall()
